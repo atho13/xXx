5ade75b58e408cd2b74a44d25d17d348d05eb56e
xXx <181945610+ribel13@users.noreply.github.com>
2026-03-06 13:03:24 +0900

    Update make-image25.sh

--- make-image25.sh
+++ make-image25.sh
@@ -38,8 +38,8 @@ build_firmware() {
     # PACKAGES + MISC + EXCLUDED + DISABLED_SERVICES    
     make image PROFILE="$target_profile" \
                #PROFILE="" PACKAGES="${my_packages}" FILES="files"
-               PACKAGES="$PACK25 $MISC $EXCLUDED" \
-               #PACKAGES="$MISC $EXCLUDED" \
+               #PACKAGES="$PACK25 $MISC $EXCLUDED" \
+               PACKAGES="$MISC $EXCLUDED" \
                FILES="$build_files"
     
     local build_status=$?
