5353a19b31001e2fa81a2bbf1c074abfb7573bd7
xXx <181945610+ribel13@users.noreply.github.com>
2026-03-18 19:55:41 +0900

    Update make-image25.sh

--- make-image25.sh
+++ make-image25.sh
@@ -7,9 +7,13 @@
 set -e
 
 # 1. Tentukan PROFILE di awal agar Validasi tidak Error
-PROFILE="${1:-generic}"
-TUNNEL_OPT="${2:-}"
-BUILD_FILES="files" # Gunakan nama folder fisik, bukan "PACKAGES"
+if [[ "$1" == "no-tunnel" ]] || [[ -z "$1" ]]; then
+   PROFILE="generic"
+   TUNNEL_OPT="$1" # Gunakan nama folder fisik, bukan "PACKAGES"
+else
+   PROFILE="$1"
+   TUNNEL_OPT="${2:-}"
+fi
 
 # Display info Image Builder
 make info
@@ -18,14 +22,14 @@ make info
 MISC=""
 EXCLUDED=""
 
-# CORE SYSTEM (Daftar paket Anda)
-PACKAGES="apk-mbedtls base-files ca-bundle dnsmasq-full dropbear e2fsprogs firewall4 fstools \
+# CORE SYSTEM (Daftar paket yang sudah dibersihkan untuk 25.12.0)
+PACKAGES="base-files ca-bundle dnsmasq-full dropbear e2fsprogs firewall4 fstools \
 grub2-bios-setup kmod-button-hotplug kmod-nft-offload libc libgcc libustream-mbedtls logd \
 mkf2fs mtd netifd nftables odhcp6c odhcpd-ipv6only partx-utils ppp ppp-mod-pppoe procd-ujail \
 uci uclient-fetch urandom-seed urngd kmod-amazon-ena kmod-amd-xgbe kmod-bnx2 kmod-dwmac-intel \
 kmod-e1000e kmod-e1000 kmod-forcedeth kmod-fs-vfat kmod-igb kmod-igc kmod-ixgbe kmod-r8169 \
-kmod-tg3 kmod-drm-i915 luci luci-compat luci-lib-base kmod-usb-net-huawei-cdc-ncm kmod-usb-net \
-kmod-usb-net-rndis luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio luci-mod-admin-full \
+kmod-tg3 luci luci-compat luci-lib-base kmod-usb-net-huawei-cdc-ncm kmod-usb-net \
+kmod-usb-net-rndis luci-lib-ip luci-lib-jsonc luci-lib-nixio luci-mod-admin-full \
 luci-mod-network kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 kmod-usb-net-asix kmod-usb-net-asix-ax88179 \
 kmod-mii luci-mod-status luci-mod-system luci-proto-3g luci-proto-mbim mbim-utils picocom minicom \
 luci-proto-ncm luci-proto-ppp luci-proto-qmi screen kmod-tun ttyd kmod-usb-atm kmod-macvlan \
